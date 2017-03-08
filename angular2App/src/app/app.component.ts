505c9a0f092104e13cb27f3b6db4069b7395e8c7
alexandru.ciocan <alexandru.ciocan@vivacredit.ro>
2017-03-08 17:50:39 +0200

    Delete folder

--- angular2App/src/app/app.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'my-app',
-  template: `<alert type="success">Well done {{name}}!</alert>`,
-})
-export class AppComponent  { name = 'Angular'; }
